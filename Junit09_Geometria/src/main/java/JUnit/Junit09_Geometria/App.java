36971f6b69d0aa91b50d015d09c2beba209cbd13
Jose <jmarinmalagon@gmail.com>
2020-08-18 00:20:45 +0200

    Update

---
+++ Junit09_Geometria/src/main/java/JUnit/Junit09_Geometria/App.java
@@ -0,0 +1,14 @@
+package JUnit.Junit09_Geometria;
+
+import JUnit.Junit09_Geometria.dto.Geometria;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Geometria gm=new Geometria();
+       gm.figura(2);
+       
+           
+    }
+}
